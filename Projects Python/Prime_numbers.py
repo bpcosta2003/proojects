f664689449183898af4e13394fa2fe5a4cb1eb20
Bruno Costa <bruno.p.costa.2003@gmail.com>
2020-08-16 22:43:07 -0300

    prime_number

---
+++ Projects Python/Prime_numbers.py
@@ -0,0 +1,11 @@
+num = int(input("Put a number to find if this number is a prime number or not: "))
+
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
