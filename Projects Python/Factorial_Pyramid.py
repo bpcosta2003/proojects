e7fff3d1acab960f459d1b55118035ddb2c800e2
Bruno Costa <bruno.p.costa.2003@gmail.com>
2020-08-18 19:01:10 -0300

    Factorial_Pyramid

--- Projects Python/Factorial_Pyramid.py
+++ Projects Python/Factorial_Pyramid.py
@@ -1,5 +1,9 @@
-range_user = int(input("Put the value of the last factorial number: "))
-for numb in range(range_user):
+range_user = int(input("\nPut the value of the last factorial number: "))
+if range_user < 0:
+    print("INVALID NUMBER")
+    exit()
+
+for numb in range(1, range_user):
 
     k = 1
     while numb > 1:
